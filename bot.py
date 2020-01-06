5924238322dcb46b77a1dab818ec07acacca8957
admind4rk <onesleep@mai.ua>
2020-01-06 22:28:56 +0200

    008

--- bot.py
+++ bot.py
@@ -1,14 +1,11 @@
 import telebot
 from telebot import types
-#from telebot import apihelper
-from flask import Flask, request
+from telebot import apihelper
 
-#apihelper.proxy = {'http':'socks5://426209941:2dpVwJ2h@orbtl.s5.opennetwork.cc:999'}
+apihelper.proxy = {'http':'socks5://426209941:2dpVwJ2h@orbtl.s5.opennetwork.cc:999'}
 bot = telebot.TeleBot('843317831:AAEAlRbKZTNcIjcSvFb1rq_Lbtv7Qtei4og')
 #os.environ['token']
 #843317831:AAEAlRbKZTNcIjcSvFb1rq_Lbtv7Qtei4og
-
-server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -28,10 +25,4 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
-@server.route('/', methods=['POST'])
-def webhook():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-if __name__ == '__main__':
-    server.run()
+bot.polling(none_stop=True)
