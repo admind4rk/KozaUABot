88e012f65466399a09594393609442d12e11bc25
admind4rk <onesleep@mai.ua>
2020-01-06 13:31:44 +0200

    005

--- bot.py
+++ bot.py
@@ -4,22 +4,11 @@ from flask import Flask, request
 
 bot = telebot.TeleBot(os.environ['token'])
 
-server = Flask{__name__}
+server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(massage):
     bot.send_message(message.chat.id, 'Alina dura, napishi poka')
-
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-
-def get_text_messages(message)
-
-if message.text == "poka":
-    bot.send_message(message.from_user.id, "Privet. Chem ya mogu pomoch'?")
-elif massage.text == "/help":
-    bot.send_message(message.from_user.id, "napishi poka")
-else:
-    bot.send_message(message.from_user.id, "napishi poka")
 
 @server.route('/', methods=['POST'])
 def webhook():
